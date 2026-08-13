a866f47f90e6320584095de4bb46d05d8cd23f23
davidfei2026 <feidavid81022@gmail.com>
2026-08-12 19:02:12 -0500

    Add files via upload

---
+++ browser-sw.js
@@ -0,0 +1,49 @@
+const CACHE = "davidcraft-private-browser-v3";
+const APP_SHELL = [
+  "./browser-v3.html",
+  "./davidcraft.css",
+  "./davidcraft.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+
+  if (request.method !== "GET") return;
+
+  const url = new URL(request.url);
+
+  if (url.origin !== self.location.origin) {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./browser-v3.html"));
+    })
+  );
+});
